c4b09b3777f612fa0cfd051084bda42ad26e2089
DevOps and Cloud Engineer Ray Eta. E. <115152999+rayeeta@users.noreply.github.com>
2024-08-25 11:53:33 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "ec2-instance" {
   ami           = "ami-0e86e20dae9224db8" # us-east-1
   instance_type = "t2.medium"
   tags = {
