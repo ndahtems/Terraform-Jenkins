1342b35aa3a80f78185002d26e997d98bcade0ce
DevOps and Cloud Engineer Ray Eta. E. <115152999+rayeeta@users.noreply.github.com>
2024-08-26 13:48:54 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-1"  
+}
+
+resource "aws_instance" "ec2-instance" {
+  ami           = "ami-0e86e20dae9224db8" # us-east-1
+  instance_type = "t2.micro"
+  tags = {
+      Name = "Jenkins-Instance"
+  }
+}
+
