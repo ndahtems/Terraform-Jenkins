8488ba297cf26b839a34106e01660c06fcbbfcd0
DevOps and Cloud Engineer Ray Eta. E. <115152999+rayeeta@users.noreply.github.com>
2024-09-01 14:58:09 +0200

    main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ec2-instance" {
   ami           = "ami-0d53d72369335a9d6" # us-west-1
   instance_type = "t2.micro"
   tags = {
-      Name = "Jenkins-Instance"
+      Name = "EKS-Jenkins-Instance"
   }
 }
 
